1aa23b1a30dab5f7f2fce2d4f1870029b465ddfb
Oli Cundale <ocundale@gmail.com>
2014-05-22 17:30:51 +0100

    added responsive grid, sample image

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,35 +1,38 @@
 "use strict";
 (function($){
+
     var s = skrollr.init({
             forceHeight: false,
             smoothScrolling: false // we set this to false since our CSS is handling the transition
         });
 
     //The options (second parameter) are all optional. The values shown are the default values.
-    // skrollr.menu.init(s, {
-    //     //skrollr will smoothly animate to the new position using `animateTo`.
-    //     animate: true,
+    skrollr.menu.init(s, {
+        //skrollr will smoothly animate to the new position using `animateTo`.
+        animate: true,
 
-    //     //The easing function to use.
-    //     easing: 'sqrt',
+        //The easing function to use.
+        easing: 'sqrt',
 
-    //     //Multiply your data-[offset] values so they match those set in skrollr.init
-    //     scale: 2,
+        //Multiply your data-[offset] values so they match those set in skrollr.init
+        scale: 2,
 
-    //     //How long the animation should take in ms.
-    //     duration: function(currentTop, targetTop) {
-    //         //By default, the duration is hardcoded at 500ms.
-    //         return 500;
+        //How long the animation should take in ms.
+        duration: function(currentTop, targetTop) {
+            console.log(currentTop, 'targ: ',targetTop);
+            //By default, the duration is hardcoded at 500ms.
 
-    //         //But you could calculate a value based on the current scroll position (`currentTop`) and the target scroll position (`targetTop`).
-    //         //return Math.abs(currentTop - targetTop) * 10;
-    //     },
+            return 500;
 
-    //     //If you pass a handleLink function you'll disable `data-menu-top` and `data-menu-offset`.
-    //     //You are in control where skrollr will scroll to. You get the clicked link as a parameter and are expected to return a number.
-    //     handleLink: function(link) {
-    //         return 400;//Hardcoding 400 doesn't make much sense.
-    //     }
-    // });
+            //But you could calculate a value based on the current scroll position (`currentTop`) and the target scroll position (`targetTop`).
+            //return Math.abs(currentTop - targetTop) * 10;
+        },
+
+        //If you pass a handleLink function you'll disable `data-menu-top` and `data-menu-offset`.
+        //You are in control where skrollr will scroll to. You get the clicked link as a parameter and are expected to return a number.
+        handleLink: function(link) {
+            //return 400;//Hardcoding 400 doesn't make much sense.
+        }
+    });
 
 } (jQuery));
